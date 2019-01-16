30a38b8c1ac102f726c265c133fca29512d375ad
CRomeroP <lituslitus16@gmail.com>
2019-01-16 13:43:25 +0100

    GameObjects upgrades
    Included config
    Created components & resources

--- PrimeraEntrega/Engine - Master/GameObject.h
+++ PrimeraEntrega/Engine - Master/GameObject.h
@@ -1,25 +1,28 @@
 #ifndef __GAMEOBJECT_H__
-#define __GAMEOBJECT_H__
+#define __GAMEOBJECT_H__M 
 
-#include "Math.h"
+#include "MathGeoLib.h"
+#include "MathBuildConfig.h"
 #include "Component.h"
+
 #include <list>
-#include <map>
 #include <vector>
 
 class GameObject
 {
+	friend class Component;
 public:
 	GameObject(GameObject* parent, const char* name);
-	GameObject(GameObject* parent, const char* name, const float3& translation, const float3& scale, const Quat& rotation);
-
-	bool Save();
-	void Load()
-
-
-
-
 	~GameObject();
+
+	Component* CreateComponent(Component::TypeComponent type);
+
+public:
+	bool active;
+	std::string name;
+	std::list<Component*> components;
+
+
 };
 
-#endif
+#endif // ! __GAMEOBJECT_H 
